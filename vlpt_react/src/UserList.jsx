16e2e99cec332150a3fb8cc5d3a6fdda3f282e6f
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-06-06 13:47:02 +0900

    feat: 배열에 항목 추가하기

--- vlpt_react/src/UserList.jsx
+++ vlpt_react/src/UserList.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
 
-function User({ user }) {
-  return (
-    <div>
-      <b>{user.username}</b> <span>({user.email})</span>
-    </div>
-  );
-}
-
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <div key={user.id}>
+          <b>{user.username}</b> <span>({user.email})</span>
+        </div>
       ))}
     </div>
   );
